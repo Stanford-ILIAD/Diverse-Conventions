5eb9abb606cd0020910007e11989c0951592ef7b
Bidipta Sarkar <bidiptas13@gmail.com>
2022-01-17 18:44:34 -0800

    Adding simple tree environment

---
+++ tree_env/__init__.py
@@ -0,0 +1,6 @@
+from gym.envs.registration import register
+
+register(
+    id='Tree-v0',
+    entry_point='tree_env.tree_evn:PantheonTree'
+)
--- tree_trainer.py
+++ tree_trainer.py
@@ -1,12 +1,33 @@
-from hanabi_env.hanabi_env import MaskedHanabi
+from tree_env.tree_env import PantheonTree
 from MAPPO.main_player import MainPlayer
+from hanabi_agent import CentralizedAgent
 from PopMAPPO.pop_player import PopPlayer
 from DMAPPO.poploss import PopulationLoss, ADAPLoss
-from hanabi_agent import CentralizedAgent
 
 from config import get_config
 import os
 from pathlib import Path
+
+# args = get_config().parse_args()
+# args.hanabi_name = 'Tree'
+# print(args)
+#
+# env = PantheonTree()
+# print(env.observation_space)
+# print(env.share_observation_space)
+# print(env.action_space)
+# run_dir = Path(os.path.split(os.path.dirname(os.path.abspath(__file__)))[0] + "/results")
+# config = {
+#     'all_args': args,
+#     'env': env,
+#     'device': 'cpu',
+#     'num_agents': 2,
+#     'run_dir': run_dir
+# }
+# ego = MainPlayer(config)
+# partner = CentralizedAgent(ego, 1)
+# env.add_partner_agent(partner)
+# ego.run()
 
 def get_loss(args):
     if args.loss_type is None:
@@ -19,25 +40,9 @@ def get_loss(args):
 
 
 def generate_player(args):
-    args.hanabi_name = 'MaskedHanabi'
-
-    han_config={
-                "colors":
-                    args.han_colors,
-                "ranks":
-                    args.han_ranks,
-                "players":
-                    2,
-                "hand_size":
-                    args.han_hand,
-                "max_information_tokens":
-                    args.han_info,
-                "max_life_tokens":
-                    args.han_life,
-                "observation_type":1
-            }
-    env = MaskedHanabi(han_config)
-    run_dir = Path(os.path.split(os.path.dirname(os.path.abspath(__file__)))[0] + "/hanabi/results/" + (args.loss_type or "standard") + "/" + str(args.seed))
+    args.hanabi_name = 'Tree'
+    env = PantheonTree()
+    run_dir = Path(os.path.split(os.path.dirname(os.path.abspath(__file__)))[0] + "/tree/results/" + (args.loss_type or "standard") + "/" + str(args.seed))
 
     print(run_dir)
     config = {
