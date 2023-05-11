db0978a4ee8656c9c969b809f2898eaa4229e896
Bidipta Sarkar <bidiptas13@gmail.com>
2023-05-11 05:12:58 -0700

    Add MultiConvention training

--- train/adap_cbr.sh
+++ train/adap_cbr.sh
@@ -9,7 +9,7 @@ export TF_ENABLE_ONEDNN_OPTS=0
 
 for p in 8 6 4
 do
-    python best_response_trainer.py --num_env_steps 1000000 --episode_length 200 --env_length 200 --use_linear_lr_decay --entropy_coef 0.0 --env_name overcooked --seed 1 --restored 0 --n_rollout_threads 50 --ppo_epoch 10 --cuda --layer_N 2 --hidden_size 64 --lr 1e-2 --critic_lr 1e-2 --over_layout $1 --run_dir adap_$p --pop_size $p --loss_type ADAP --loss_param 0.025
+    python best_response_trainer.py --num_env_steps 200000 --episode_length 200 --env_length 200 --use_linear_lr_decay --entropy_coef 1e-3 --env_name overcooked --seed 1 --restored 0 --n_rollout_threads 50 --ppo_epoch 100 --cuda --layer_N 2 --hidden_size 64 --lr 1e-2 --critic_lr 1e-2 --over_layout $1 --run_dir adap_$p --pop_size $p --loss_type ADAP --loss_param 0.025
     
     # python torch_to_tfjs.py --env_name overcooked --seed 1 --n_rollout_threads 1 --layer_N 2 --hidden_size 64 --over_layout $r --run_dir sp --ai_name SP
 done
--- train/best_response_trainer.py
+++ train/best_response_trainer.py
@@ -5,9 +5,9 @@ from env_utils import generate_env, set_seed
 
 import torch
 
-from XD.MCPolicy import MCPolicy
-from XD.xd_player import XDPlayer
+from MultiConvention.xd_player import XDPlayer
 from MAPPO.utils.shared_buffer import SharedReplayBuffer
+from MAPPO.rMAPPOPolicy import R_MAPPOPolicy
 
 # import torch
 import numpy as np
@@ -34,25 +34,21 @@ def run_serial(N, args, env_generator, base_dir, device, threads, restored=0):
     args.use_recurrent_policy = False
     args.use_average = True
     for agent_num in range(N + 1):
-        env = env_generator(threads * ((agent_num) * 2 + 1))
-        env_mp = env_generator(args.env_length - 1)
+        env = env_generator(threads * (agent_num + 1))
         set_rands(args.seed + args.seed_skip * agent_num)
         print("Training agent", agent_num)
         if agent_num == N:
             args.use_recurrent_policy = true_recurrence
-        next_agent = MCPolicy(
+        next_agent = R_MAPPOPolicy(
             args,
             env.observation_space,
             env.share_observation_space,
             env.action_space,
-            agent_num,
             torch.device(device),
         )
 
         sp_buf = generate_buffer(args, env, device)
-        xp_buf0 = [generate_buffer(args, env, device) for _ in range(agent_num)]
-        xp_buf1 = [generate_buffer(args, env, device) for _ in range(agent_num)]
-        mp_buf = generate_buffer(args, env_mp, device, args.env_length - 1)
+        xp_buf = [generate_buffer(args, env, device) for _ in range(agent_num)]
 
         if agent_num == N:
             run_dir = Path(base_dir + "/oracle_" + str(N) + ("_r" if true_recurrence else ""))
@@ -62,7 +58,6 @@ def run_serial(N, args, env_generator, base_dir, device, threads, restored=0):
         config = {
             "all_args": args,
             "envs": env,
-            "envs_mp": env_mp,
             "device": device,
             "num_agents": 2,
             "run_dir": run_dir,
@@ -73,13 +68,9 @@ def run_serial(N, args, env_generator, base_dir, device, threads, restored=0):
             config,
             next_agent,
             sp_buf,
-            xp_buf0,
-            xp_buf1,
-            mp_buf,
+            xp_buf,
             agent_set,
             args.xp_weight,
-            args.mp_weight,
-            args.mix_prob,
             args.env_length,
         )
         if agent_num < N:
@@ -130,92 +121,3 @@ with open(run_dir + "/" + "args.txt", "w", encoding="UTF-8") as file:
     file.write(str(args))
 
 run_serial(pop_size, args, env_generator, run_dir, device, args.n_rollout_threads, restored=args.restored)
-
-
-
-
-
-# import os
-# from config import get_config
-# import torch
-# import numpy as np
-# import random
-
-# from pathlib import Path
-
-# from env_utils import generate_env, set_seed
-
-# from BestResponse.oracle_player import OraclePlayer
-# from XD.MCPolicy import MCPolicy
-# from XD.xd_player import XDPlayer
-
-# from MAPPO.utils.shared_buffer import SharedReplayBuffer
-# from MAPPO.r_actor_critic import R_Actor
-
-
-# def set_rands(seed):
-#     torch.manual_seed(seed)
-#     random.seed(seed)
-#     np.random.seed(seed)
-
-# def get_dir(args):
-#     if args.loss_type:
-#         return (
-#             os.path.dirname(os.path.abspath(__file__))
-#             + "/results/"
-#             + args.hanabi_name
-#             + "/baselines/"
-#             + args.loss_type
-#             + "/"
-#             + (args.run_dir)
-#             + "/"
-#             + str(args.seed)
-#         )
-#     else:
-#         return (
-#             os.path.dirname(os.path.abspath(__file__))
-#             + "/results/"
-#             + args.hanabi_name
-#             + "/"
-#             + (args.run_dir)
-#             + "/"
-#             + str(args.seed)
-#         )
-
-# def main():
-#     args = get_config().parse_args()
-#     print(args)
-#     pop_size = args.pop_size
-#     device = "cuda" if torch.cuda.is_available() and args.cuda else 'cpu'
-#     env = generate_env(args.env_name, args.n_rollout_threads * (pop_size * 2 + 1), args.over_layout, use_env_cpu=(device=='cpu'))
-
-#     args.hanabi_name = args.over_layout if args.env_name == 'overcooked' else args.env_name
-    
-#     base_dir = get_dir(args)
-#     os.makedirs(base_dir, exist_ok=True)
-
-#     agent_set = []
-#     set_rands(args.seed)
-#     true_recurrence = args.use_naive_recurrent_policy
-#     args.use_naive_recurrent_policy = False
-#     for agent_num in range(pop_size):
-#         actor = R_Actor(args, env.observation_space, env.action_space)
-#         state_dict = torch.load(base_dir + "/convention" + str(agent_num) +"/models/actor.pt")
-#         actor.load_state_dict(state_dict)
-#         agent_set.append(actor)
-#     args.use_naive_recurrent_policy = true_recurrence
-
-#     run_dir = Path(base_dir + "/oracle_" + str(pop_size) + ("_r" if true_recurrence else ""))
-#     config = {
-#         "all_args": args,
-#         "envs": env,
-#         "device": device,
-#         "num_agents": 2,
-#         "run_dir": run_dir,
-#     }
-#     ego = OraclePlayer(config, agent_set)
-#     ego.run()
-
-
-# if __name__ == "__main__":
-#     main()
